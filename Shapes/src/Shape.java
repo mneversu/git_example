37aef3a1ba51cc21b3c2a7e00e10c6678fb2866b
manish <mneversu@asu.edu>
2012-12-02 17:37:01 -0700

    Version 2 Added Line in Shapes Class As version 2 updated locally

--- Shapes/src/Shape.java
+++ Shapes/src/Shape.java
@@ -6,6 +6,7 @@ public class Shape
  public Shape()
  {
    System.out.println("Version 1: This is updated");
+   System.out.println("Version 2: Updated in Local");
    area=perimeter=0;
  }
   public String toString()
